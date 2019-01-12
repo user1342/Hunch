51832780940a61210fdc0db8da8a1281bc64b634
James <james.l.stevenson@gmail.com>
2019-01-12 11:44:47 +0000

    Adding core tools.

---
+++ Core_Aggregator.py
@@ -0,0 +1,87 @@
+import json
+
+'''
+A class used to define which aggregation sources will be used to gather text from individuals
+as well as what handles / account names will be gathered. 
+'''
+class WebsiteToCrawl:
+
+    # The default constructor
+    def __init__(self):
+        self.website_names = []
+
+        self.handles = []
+
+        self.list_of_text = []
+
+        self.name = ""
+
+    #This is used to add items to the website list and make sure that there are no duplicates
+    def add_to_website_list(self, website):
+        try:
+            self.website_names.extend(website)
+        except:
+            self.website_names.append(website)
+        self.website_names = list(set(self.website_names))
+
+    #This is used to add items to the handles list and make sure that there are no duplicates
+    def add_to_handles_list(self, handles):
+        try:
+            self.handles.extend(handles)
+        except:
+            self.handles.append(handles)
+        self.handles = list(set(self.handles))
+
+    #This file takes a file location as an input and reads that file as json and adds the contents to the necessary fields.
+    def read_from_json_file(self, file_path):
+
+        with open(file_path) as json_file:
+            json_input = json.load(json_file)
+
+            for key in json_input:
+                value = json_input[key]
+
+                for attribute in value:
+                    nested_value = value[attribute]
+
+                    if attribute == "websites":
+                        self.add_to_website_list(nested_value)
+                    elif attribute == "handles":
+                        self.add_to_handles_list(nested_value)
+                    elif attribute == "name":
+                        self.name = nested_value
+                    else:
+                        raise Exception("Json in incorrect form for profiling.")
+
+
+    # This function checks what websites have been requested to profile users and what
+    # user accounts are to be profiled on the websites.
+    def aggregate_data(self):
+        assert self.website_names is not None, " No websites listed"
+        assert self.list_of_text is not None, "No hndles listed"
+
+        #Loops through the requested websites
+        for website in self.website_names:
+            website = website.lower()
+
+            #Checks what aggrigator to use based on the website chosen
+            if website == "twitter":
+                from Data_Source_Aggregators import Twitter_Aggrigator as tw
+
+                aggrigator = tw.Twitter_Aggrigator()
+
+                #Loops through each of the handles for each of the websites
+                for user in self.handles:
+                    response = aggrigator.pull_from_twitter(user)
+                    try:
+                        self.list_of_text.extend(response)
+                    except:
+                        self.list_of_text.append(response)
+
+            elif website == "reddit":
+                pass
+
+            else:
+                raise Exception("Unknown Website " + website)
+
+        return self.list_of_text
---
+++ Core_Individual.py
@@ -0,0 +1,74 @@
+import random
+
+'''
+A class that is used to build a knowledge base on a likelihood.
+'''
+class Individual:
+
+    #The constructor, setting class variables for each individual
+    def __init__(self):
+        # A score of this individuals liklihood
+        self._liklihood = None
+        # Name of the individual
+        self.name = ""
+        # A list used to hold the text that will be profiled
+        self._text_to_be_profiled = []
+
+    def get_liklihood(self):
+        assert self._liklihood is not None, " Liklihood has not been set."
+        return self._liklihood
+
+    #A method used to add text to the aformentioned list
+    def add_text_to_be_profiled(self,text):
+        self._text_to_be_profiled.append(text)
+
+    #A method used to remove text from the aformentioned list
+    def remove_from__text_to_be_profiled(self,text):
+        self._text_to_be_profiled.remove(text)
+
+    # A method that generates a psudo random name for the individual
+    def generate_name(self, filename = "Core_wordfile", words_in_name = 3):
+        list_of_words = []
+
+        file = open(filename, "r")
+        for line in file:
+            list_of_words.append(line.strip("\n"))
+
+        for iterator in range(words_in_name):
+            random_number = random.randint(0, len(list_of_words))
+            self.name = self.name + list_of_words[random_number-1]+"//"
+
+    #Uses the detectors to calculate a risk score for the individual
+    def profile(self):
+        assert self.name is not "", "Individual has no name"
+        assert self._text_to_be_profiled, "List of text to be profiled is empty"
+
+        list_of_detectors = []
+
+        # Adds the Relationship Detector to be used when profiling
+        from Liklihood_Detectors import Relationship_Detection as rd
+        relationship_profile = rd.Relationship_Detection()
+        list_of_detectors.append(relationship_profile)
+
+        # Adds the goal detector to be used when profiling
+        from Liklihood_Detectors import Goal_Detection as gd
+        goal_profile = gd.Goal_Detection()
+        list_of_detectors.append(goal_profile)
+
+        total_scores = []
+        total = 0
+        # Loops through all of the detectors
+        for detector in list_of_detectors:
+
+            #Loops through the list of text to be profiled, profiling each, and then calculating an average
+            for text in self._text_to_be_profiled:
+
+                total_scores.append(detector.get_score(text))
+
+        for number in total_scores:
+            total = total + number
+        average = total / len(total_scores)
+        self._liklihood = average
+
+        return self._liklihood
+
