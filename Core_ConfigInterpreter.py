18da106876caad82d92bb449618bca8f558a0b6e
James <james.l.stevenson@gmail.com>
2019-02-24 12:50:25 +0000

    Minor feature changes.

--- Core_ConfigInterpreter.py
+++ Core_ConfigInterpreter.py
@@ -7,33 +7,6 @@ A class used to check default settings for Hunch as well as settings from a defi
 '''
 class Config:
 
-    #Default constructor
-    def __init__(self):
-
-        # Maybe instead of the dictionary add mini functions for each. The just have them call the dictionary
-        self._dictionary_of_config_paramiters = {
-            "default_analyser": AWSComprehend.AWSComprehend(),
-            "score_high": 10,
-            "score_medium": 5,
-            "score_low": 0,
-            "default_impact": 1,
-            "default_aggregations": 10,
-            "character_limit":250,
-
-            "reddit_client_id": None,
-            "reddit_client_secret" : None,
-            "reddit_username" : None,
-            "reddit_password" : None,
-            "reddit_user_agent" : "Hunch",
-
-            "twitter_consumer_key" : None,
-            "twitter_consumer_secret" : None,
-            "twitter_access_token" : None,
-            "twitter_access_token_secret" : None,
-
-            "blacklisted_strings": []
-        }
-
     #The below functions are used to get api information, such as Twitter and Reddit API keys
 
     def get_twitter_consumer_key (self, config = None):
@@ -129,8 +102,7 @@ class Config:
         paramiter = paramiter.lower()
 
         if config_file_location is None or os.path.isfile(config_file_location) is False:
-            if cc.Config().get_verbose("core_config.json"): print("No file given or file given incorrect, defaulting to default config.")
-            ret_val = self._read_default_paramiter(paramiter)
+            raise Exception("No file given or file given incorrect")
         else:
             ret_val = self._read_from_file(paramiter,config_file_location)
 
@@ -145,13 +117,9 @@ class Config:
                 ret_val = data["config"][paramiter]
 
         except:
-            raise Exception ("Failed to load JSON.")
+            raise Exception ("Failed to load JSON configuration.")
 
         return ret_val
-
-    #This is called if no valid config file is given and the defaults are required
-    def _read_default_paramiter(self, paramiter):
-        return self._dictionary_of_config_paramiters[paramiter]
 
     # Re qrites a specific paramiter in the config file
     def set_paramiter_in_config_in_file(self, config_file_path, paramiter, value):
--- Example_Profile_Script.py
+++ Example_Profile_Script.py
@@ -2,10 +2,8 @@ import os
 import Core_Aggregator
 import Core_Individual
 import Core_Display
-import Core_ConfigInterpreter as cc
 
 # Loops through the json folder and creates an individual to profile based off each file.
-
 if __name__ == '__main__':
     json_file_directory = r"Json_Files"
 
@@ -15,17 +13,17 @@ if __name__ == '__main__':
         my_aggrigator = Core_Aggregator.WebsiteToCrawl()
         full_path = os.path.join(json_file_directory, filename)
         my_aggrigator.read_from_json_file(full_path)
-        if cc.Config().get_verbose("core_config.json"): print(
+        print(
             "\n\nCreated Aggrigator for  " + my_aggrigator.name + ": " + str(my_aggrigator.list_of_dictionary_sources))
 
         my_individual = Core_Individual.Individual(my_aggrigator.aggregate_data())
         my_individual.impact = my_aggrigator.impact
         my_individual.name = my_aggrigator.name  # my_individual.generate_name()
-        if cc.Config().get_verbose("core_config.json"): print("Beginning profiling " + my_individual.name + "'s " + str(
+        print("Beginning profiling " + my_individual.name + "'s " + str(
             len(my_individual._text_to_be_profiled)) + " samples.")
 
         results = my_individual.profile()
-        if cc.Config().get_verbose("core_config.json"): print("Risk " + str(results["risk"]) + "| Impact " + str(results["impact"]) + "| Likelihood " + str(
+        print("Risk " + str(results["risk"]) + "| Impact " + str(results["impact"]) + "| Likelihood " + str(
             results["likelihood"]))
 
         list_of_profiled_individuals.append(results)
