d139d9cccfa1a4c032df72aa7a95ac5c83d8be4b
James <james.l.stevenson@gmail.com>
2019-03-03 16:48:07 +0000

    Adding recursive on JSON folder.

--- Example_Profile_Script.py
+++ Example_Profile_Script.py
@@ -2,31 +2,35 @@ import os
 import CORE_Collector
 import CORE_Individual
 import CORE_Display
-
-# Loops through the json folder and creates an individual to profile based off each file.
 import CORE_Logger
 
+# Loops through the json folder and creates an individual to profile based off each file.
 if __name__ == '__main__':
     json_file_directory = r"Json_Files"
 
     list_of_profiled_individuals = []
 
-    for filename in os.listdir(json_file_directory):
-        my_collector = CORE_Collector.WebsiteToCrawl()
-        full_path = os.path.join(json_file_directory, filename)
-        my_collector.read_from_json_file(full_path)
-        CORE_Logger.log("Created Collector for  " + my_collector.name + ": " + str(my_collector.list_of_dictionary_sources))
+    for subdir, dirs, files in os.walk(json_file_directory):
+        for filename in files:
+            if filename.endswith(".json"):
+                my_collector = CORE_Collector.WebsiteToCrawl()
+                full_path = os.path.join(subdir, filename)
 
-        my_individual = CORE_Individual.Individual(my_collector.aggregate_data())
-        my_individual.impact = my_collector.impact
-        my_individual.name = my_collector.name  # my_individual.generate_name()
-        CORE_Logger.log("Beginning profiling " + my_individual.name + "'s " + str(
-            len(my_individual._text_to_be_profiled)) + " samples.")
+                my_collector.read_from_json_file(full_path)
 
-        results = my_individual.profile()
-        CORE_Logger.log("Risk " + str(results["risk"]) + "| Impact " + str(results["impact"]) + "| Likelihood " + str(
-            results["likelihood"]))
+                CORE_Logger.log("Profiling JSON file: " + full_path)
+                CORE_Logger.log("Created Collector for  " + my_collector.name + ": " + str(my_collector.list_of_dictionary_sources))
 
-        list_of_profiled_individuals.append(results)
+                my_individual = CORE_Individual.Individual(my_collector.aggregate_data())
+                my_individual.impact = my_collector.impact
+                my_individual.name = my_collector.name  # my_individual.generate_name()
+                CORE_Logger.log("Beginning profiling " + my_individual.name + "'s " + str(
+                    len(my_individual._text_to_be_profiled)) + " samples.")
+
+                results = my_individual.profile()
+                CORE_Logger.log("Risk " + str(results["risk"]) + "| Impact " + str(results["impact"]) + "| Likelihood " + str(
+                    results["likelihood"]))
+
+                list_of_profiled_individuals.append(results)
 
     CORE_Display.create_website(list_of_profiled_individuals).generate_page()
